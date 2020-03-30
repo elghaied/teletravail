0a63238292d8c0b58510b8f45b247e2a210fe1a2
Eslam <elghaied.eslam@gmail.com>
2020-03-30 16:16:41 +0200

    overflow

--- navigation.js
+++ navigation.js
@@ -1,8 +1,10 @@
-const toggleMenu = document.querySelector('.navigation button');
-const menu = document.querySelector('.navigation ul');
+const toggleMenu = document.querySelector(".navigation button");
+const menu = document.querySelector(".navigation ul");
+const noscroll = document.querySelector(" .noscroll");
 
-toggleMenu.addEventListener('click', function() {
-	const open = JSON.parse(toggleMenu.getAttribute('aria-expanded'));
-	toggleMenu.setAttribute('aria-expanded', !open);
-	menu.hidden = !menu.hidden;
+toggleMenu.addEventListener("click", function() {
+  const open = JSON.parse(toggleMenu.getAttribute("aria-expanded"));
+  toggleMenu.setAttribute("aria-expanded", !open);
+  menu.hidden = !menu.hidden;
+  noscroll.style.overflow = "hidden";
 });
